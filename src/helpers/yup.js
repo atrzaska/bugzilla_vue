d9fb531d65886a0d6d0de4217f22fbfd4c958004
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-18 16:22:08 +0200

    Validate max length of the inputs

--- src/helpers/yup.js
+++ src/helpers/yup.js
@@ -1,16 +1,16 @@
 import * as yup from 'yup'
 
 const signUpSchema = yup.object().shape({
-  name: yup.string().required().min(8),
-  email: yup.string().email(),
-  password: yup.string().min(8),
+  name: yup.string().required().min(8).max(255),
+  email: yup.string().email().max(255),
+  password: yup.string().min(8).max(255),
   termsAccepted: yup.boolean().required(),
   newsletterSubscribed: yup.boolean(),
 })
 
 const signInSchema = yup.object().shape({
-  email: yup.string().email(),
-  password: yup.string().min(8),
+  email: yup.string().email().max(255),
+  password: yup.string().min(8).max(255),
 })
 
 export { signUpSchema, signInSchema }
