0c2b5f5c6a94256f75a19986f124dee9ab43d12f
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-18 13:22:38 +0200

    Add frontend validation for signup

--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -16,7 +16,10 @@
           placeholder="Name"
           type="text"
           required
+          @blur="validateField('name', name)"
+          @input="validateField('name', name)"
         />
+        <div v-if="errors.name" class="invalid-feedback">{{ errors.name }}</div>
         <input
           v-model="email"
           autocomplete="username"
@@ -28,7 +31,12 @@
           placeholder="Email address"
           type="email"
           required
+          @blur="validateField('email', email)"
+          @input="validateField('email', email)"
         />
+        <div v-if="errors.email" class="invalid-feedback">
+          {{ errors.email }}
+        </div>
         <input
           v-model="password"
           autocomplete="new-password"
@@ -40,16 +48,29 @@
           placeholder="Password 8+ characters"
           type="password"
           required
+          @blur="validateField('password', password)"
+          @input="validateField('password', password)"
         />
+        <div v-if="errors.password" class="invalid-feedback">
+          {{ errors.password }}
+        </div>
       </div>
       <div class="form-group">
         <label class="d-block font-weight-normal text-secondary">
-          <input v-model="termsAccepted" type="checkbox" required />
+          <input
+            v-model="termsAccepted"
+            type="checkbox"
+            @blur="validateField('termsAccepted', termsAccepted)"
+            @click="validateField('termsAccepted', termsAccepted)"
+          />
           &nbsp;I agree to Bugzilla&nbsp;
           <a href="/terms" target="_blank">Terms&nbsp;</a>
           and&nbsp;
           <a href="/privacy" target="_blank">Privacy Policy</a>
         </label>
+        <div v-if="errors.termsAccepted" class="invalid-feedback">
+          {{ errors.termsAccepted }}
+        </div>
       </div>
       <div class="form-group">
         <label class="d-block font-weight-normal text-secondary">
@@ -58,7 +79,11 @@
         </label>
       </div>
       <div class="form-group">
-        <button class="btn btn-lg btn-primary btn-block" type="submit">
+        <button
+          class="btn btn-lg btn-primary btn-block"
+          type="submit"
+          :disabled="!isValid"
+        >
           Get started now
         </button>
       </div>
@@ -67,21 +92,25 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import AuthLayout from '@/layouts/Auth'
 import ErrorsBox from '@/components/ErrorsBox'
 import useSignup from '@/use/useSignup'
-import useValidation from '@/use/useValidation'
+import useFrontendValidation from '@/use/useFrontendValidation'
+import { signUpSchema } from '@/helpers/yup'
 
-const [
+const errors = ref({})
+const {
   name,
   email,
   password,
   termsAccepted,
   newsletterSubscribed,
-  errors,
-  onSubmit,
-] = useSignup()
-const validation = useValidation(errors)
-const { invalidFieldClass } = validation
+  payload,
+  signUp,
+} = useSignup(errors)
+const validation = useFrontendValidation({ errors, schema: signUpSchema })
+const { isValid, invalidFieldClass, validateField, validateForm } = validation
+// TODO: checbox validation is not working
+const onSubmit = () => validateForm(payload()) && signUp()
 </script>
