a9662cac8c279a285219418df7b54701ed422ff3
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-18 13:20:35 +0200

    Add toast helper

---
+++ src/lib/toast.js
@@ -0,0 +1,30 @@
+import Swal from 'sweetalert2'
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 5000,
+  timerProgressBar: false,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  },
+})
+
+const success = (title) => Toast.fire({ icon: 'success', title })
+const error = (title) =>
+  Toast.fire({
+    icon: 'error',
+    title,
+    showClass: {
+      popup: 'animate__animated animate__slideInRight',
+    },
+    hideClass: {
+      popup: 'animate__animated animate__slideOutRight',
+    },
+  })
+const warning = (title) => Toast.fire({ icon: 'warning', title })
+const info = (title) => Toast.fire({ icon: 'info', title })
+
+export default { success, error, warning, info }
