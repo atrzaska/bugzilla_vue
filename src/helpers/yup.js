98f5f8431ac00c81ff1a850c8b5bc0436b74a3d4
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-18 22:32:06 +0200

    Fix terms validation

--- src/helpers/yup.js
+++ src/helpers/yup.js
@@ -4,7 +4,10 @@ const signUpSchema = yup.object().shape({
   name: yup.string().required().min(8).max(255),
   email: yup.string().email().max(255),
   password: yup.string().min(8).max(255),
-  termsAccepted: yup.boolean().required(),
+  termsAccepted: yup
+    .boolean()
+    .required()
+    .oneOf([true], 'The terms and conditions must be accepted.'),
   newsletterSubscribed: yup.boolean(),
 })
 
