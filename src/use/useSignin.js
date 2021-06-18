94bd51139e7c96ea9896019e555521d0e34717a2
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-18 22:37:51 +0200

    Add rememberMe ref to useSignin

--- src/use/useSignin.js
+++ src/use/useSignin.js
@@ -4,13 +4,14 @@ import API from '@/services/requests'
 const useSignin = () => {
   const email = ref('')
   const password = ref('')
+  const rememberMe = ref(false)
   const error = ref(false)
   const onSubmit = () => {
     error.value = false
     API.signIn(email.value, password.value).catch((err) => (error.value = true))
   }
 
-  return { email, password, error, onSubmit }
+  return { email, password, rememberMe, error, onSubmit }
 }
 
 export default useSignin
