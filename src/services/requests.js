4defb6ad3304c5621561a97897b593e7ba8b2acf
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-21 00:25:58 +0200

    Add rest requests to all objects

--- src/services/requests.js
+++ src/services/requests.js
@@ -5,6 +5,7 @@ import API from '@/lib/api'
 const clearToken = () => {
   window.localStorage.removeItem('authToken')
   router.push('/signin')
+  API.clearCache('/me')
 }
 
 const refreshToken = async () => {
@@ -13,19 +14,11 @@ const refreshToken = async () => {
   }
 
   API.get('/refresh_token')
-    .then((res) => {
-      const newToken = res.data.token
-      window.localStorage.setItem('authToken', newToken)
-    })
+    .then((res) => window.localStorage.setItem('authToken', res.data.token))
     .catch(clearToken)
 }
-const fetchCurrentUser = async () => {
-  if (!window.localStorage.getItem('authToken')) {
-    return
-  }
 
-  return API.get('/me').catch(clearToken)
-}
+const fetchCurrentUser = async () => API.get('/me')
 
 const signIn = ({ email, password, rememberMe }) => {
   return API.post('/signin', { email, password, rememberMe }).then((res) => {
@@ -33,6 +26,7 @@ const signIn = ({ email, password, rememberMe }) => {
     window.localStorage.setItem('authToken', token)
     store.dispatch('setUser', user)
     router.push('/dashboard')
+    API.clearCache('/me')
     return { token, user }
   })
 }
@@ -43,15 +37,43 @@ const signUp = (user) =>
     return res
   })
 const logout = () => API.post('/logout').then(clearToken).catch(clearToken)
-const fetchProjects = (params) => API.get('/projects', { params })
+
+const fetchProjects = (params, options = {}) =>
+  API.get('/projects', Object.assign({ params }, options))
 const fetchProject = (id) => API.get(`/projects/${id}`)
+const createProject = (params) => API.post('/projects', params)
+const updateProject = (id, params) => API.put(`/projects/${id}`, params)
+const deleteProject = (id) => API.delete(`/projects/${id}`)
 const fetchProjectBySlug = (slug) =>
   fetchProjects({ 'filter.slug': slug }).then((res) => res.data.collection[0])
+
 const fetchStories = (params, options = {}) =>
   API.get('/stories', Object.assign({ params }, options))
 const fetchStory = (id) => API.get(`/stories/${id}`)
+const createStory = (params) => API.post('/stories', params)
 const updateStory = (id, params) => API.put(`/stories/${id}`, params)
 const deleteStory = (id) => API.delete(`/stories/${id}`)
+
+const fetchComments = (params, options = {}) =>
+  API.get('/stories', Object.assign({ params }, options))
+const fetchComment = (id) => API.get(`/comments/${id}`)
+const createComment = (params) => API.post('/comments', params)
+const updateComment = (id, params) => API.put(`/comments/${id}`, params)
+const deleteComment = (id) => API.delete(`/comments/${id}`)
+
+const fetchTasks = (params, options = {}) =>
+  API.get('/stories', Object.assign({ params }, options))
+const fetchTask = (id) => API.get(`/tasks/${id}`)
+const createTask = (params) => API.post('/tasks', params)
+const updateTask = (id, params) => API.put(`/tasks/${id}`, params)
+const deleteTask = (id) => API.delete(`/tasks/${id}`)
+
+const fetchMembers = (params, options = {}) =>
+  API.get('/stories', Object.assign({ params }, options))
+const fetchMember = (id) => API.get(`/members/${id}`)
+const createMember = (params) => API.post('/members', params)
+const updateMember = (id, params) => API.put(`/members/${id}`, params)
+const deleteMember = (id) => API.delete(`/members/${id}`)
 
 export default {
   clearToken,
@@ -60,11 +82,34 @@ export default {
   signIn,
   signUp,
   logout,
+
   fetchProjects,
   fetchProject,
+  createProject,
+  updateProject,
+  deleteProject,
   fetchProjectBySlug,
+
   fetchStories,
   fetchStory,
   updateStory,
   deleteStory,
+
+  fetchComments,
+  fetchComment,
+  createComment,
+  updateComment,
+  deleteComment,
+
+  fetchTasks,
+  fetchTask,
+  createTask,
+  updateTask,
+  deleteTask,
+
+  fetchMembers,
+  fetchMember,
+  createMember,
+  updateMember,
+  deleteMember,
 }
