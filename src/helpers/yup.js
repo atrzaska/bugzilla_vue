094344dccd029e12380384355ee77e07972dd23f
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-21 00:26:14 +0200

    Add validation for new project

--- src/helpers/yup.js
+++ src/helpers/yup.js
@@ -16,4 +16,8 @@ const signInSchema = yup.object().shape({
   password: yup.string().min(8).max(255),
 })
 
-export { signUpSchema, signInSchema }
+const createProjectSchema = yup.object().shape({
+  name: yup.string().required().max(255),
+})
+
+export { signUpSchema, signInSchema, createProjectSchema }
