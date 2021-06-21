21979e9e697f9ea29464dbea0b757c5c1021a853
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-21 23:21:33 +0200

    Add modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,72 @@
+<template>
+  <div v-if="modal.show" class="modal-backdrop fade show"></div>
+  <transition name="fade">
+    <div
+      v-if="modal.show"
+      class="modal fade show"
+      id="exampleModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div v-if="!!modal.title" class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">
+              {{ modal.title }}
+            </h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              @click="onClose"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div v-if="!!modal.body" class="modal-body">
+            {{ modal.body }}
+          </div>
+          <div v-if="modal.showCancel || !!modal.confirm" class="modal-footer">
+            <button
+              v-if="modal.showCancel"
+              type="button"
+              class="btn btn-outline-secondary"
+              @click="modal.onClose"
+            >
+              Cancel
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary"
+              @click="modal.onConfirm"
+            >
+              {{ modal.confirm }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script setup>
+import useModal from '@/use/useModal'
+
+const { Modal, modal } = useModal()
+window.Modal = Modal
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/use/useModal.js
@@ -0,0 +1,36 @@
+import { reactive } from 'vue'
+
+const useModal = () => {
+  const modal = reactive({
+    show: false,
+    title: 'Modal title',
+    body: 'Modal message',
+    showCancel: true,
+    confirm: 'Confirm',
+    onClose: () => (modal.show = false),
+    onConfirm: () => modal.onClose(),
+  })
+
+  const show = (options) => {
+    modal.show = true
+    modal.title = options.title || ''
+    modal.body = options.body || ''
+    modal.showCancel = options.showCancel || true
+    modal.confirm = options.confirm || 'Confirm'
+    modal.onConfirm = () => {
+      modal.onClose()
+      options.onConfirm && options.onConfirm()
+    }
+  }
+
+  const Modal = {
+    show,
+    confirm: show,
+    ok: ({ title, body, onConfirm = () => {} }) =>
+      show({ title, body, onConfirm, showCancel: false, confirm: 'OK' }),
+  }
+
+  return { Modal, modal }
+}
+
+export default useModal
