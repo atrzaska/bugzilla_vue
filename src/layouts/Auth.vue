74d2d0eeb545e6a9f168cc375d32f4cc1f2f4aa3
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-22 16:22:03 +0200

    No need to fetch user in auth layout

--- src/layouts/Auth.vue
+++ src/layouts/Auth.vue
@@ -6,9 +6,5 @@
 <script setup>
 import AuthHeader from '@/components/AuthHeader'
 import { onMounted } from 'vue'
-import API from '@/services/requests'
-
-API.refreshToken()
-API.fetchCurrentUser()
 document.body.className = 'bg-white'
 </script>
