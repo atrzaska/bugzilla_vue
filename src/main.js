16473b79964540e2a8eea50c0ce7e5064f35b8f2
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-23 11:28:23 +0200

    Load scss first to allow overriding styles in vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
+import '@/plugins'
+import 'animate.css/animate.css'
+import '@/css/app.scss'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
-import '@/plugins'
-import 'animate.css/animate.css'
-import '@/css/app.scss'
 
 const app = createApp(App)
 app.use(store).use(router).mount('#app')
