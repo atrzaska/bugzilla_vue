3caddf4c0fb8669694f8400679e357dfe1d358f8
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-26 23:27:42 +0200

    Fix jumbotron style in bootstrap 5

--- src/views/Pages/Landing.vue
+++ src/views/Pages/Landing.vue
@@ -1,28 +1,30 @@
 <template>
   <LandingLayout>
-    <div class="jumbotron bg-white text-center mx-auto product-header">
-      <h1 class="display-4 pb-4">
-        Bugzilla, simple and effective project management for your team
-      </h1>
-      <p class="lead pb-5 text-secondary">
-        Collaborate with your remote team to improve what you do, guided by a
-        simple workflow. And for free!
-      </p>
-      <div class="d-flex justify-content-center">
-        <router-link
-          class="btn btn-primary btn-lg py-3 px-5"
-          to="/signup"
-          role="button"
-        >
-          Get started
-        </router-link>
-        <router-link
-          class="btn btn-outline-primary btn-lg py-3 px-5 ms-3"
-          to="#howitworks"
-          role="button"
-        >
-          See how it works
-        </router-link>
+    <div class="p-4 mb-3">
+      <div class="jumbotron bg-white text-center mx-auto product-header py-5">
+        <h1 class="display-4 pb-4">
+          Bugzilla, simple and effective project management for your team
+        </h1>
+        <p class="lead pb-5 text-secondary">
+          Collaborate with your remote team to improve what you do, guided by a
+          simple workflow. And for free!
+        </p>
+        <div class="d-flex justify-content-center">
+          <router-link
+            class="btn btn-primary btn-lg py-3 px-5"
+            to="/signup"
+            role="button"
+          >
+            Get started
+          </router-link>
+          <router-link
+            class="btn btn-outline-primary btn-lg py-3 px-5 ms-3"
+            to="#howitworks"
+            role="button"
+          >
+            See how it works
+          </router-link>
+        </div>
       </div>
     </div>
     <footer class="text-center">
