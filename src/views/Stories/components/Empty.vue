174f58b96081cd1b6c7b9cefe5532eaeedbe539c
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-26 09:55:17 +0200

    Change h1 to h5 in stories empty sfc

--- src/views/Stories/components/Empty.vue
+++ src/views/Stories/components/Empty.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-center text-secondary p-4">You don't have any stories yet</h1>
+  <h5 class="text-center text-secondary p-4">You don't have any stories yet</h5>
   <div class="text-center">
     <router-link class="btn btn-primary" :to="`/projects/${id}/stories/new`">
       Create story
