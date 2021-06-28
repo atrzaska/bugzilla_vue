544b8355bc4949c63194268bfe7f91ff156da41e
Andrzej Trzaska <atrzaska2@gmail.com>
2021-06-28 22:25:24 +0200

    Remove utils

--- src/helpers/utils.js
+++
@@ -1,6 +0,0 @@
-const fullName = ({ firstName, lastName }) =>
-  [firstName, lastName].filter((x) => Boolean(x)).join(' ')
-
-const scrollToTop = () => window.scrollTo(0, 0)
-
-export { fullName, scrollToTop }
