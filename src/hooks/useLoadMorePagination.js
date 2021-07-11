6cf4c29dbdb8b1f7d8518a571bf04fa0aca41c75
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-11 21:40:45 +0200

    Remove setCollection from useLoadMorePagination

--- src/hooks/useLoadMorePagination.js
+++ src/hooks/useLoadMorePagination.js
@@ -1,26 +1,13 @@
 import { ref, computed } from 'vue'
 
-const SIZE = 10
-
 const useLoadMorePagination = (options = {}) => {
-  const size = options.size || SIZE
-  const collection = options.collection || ref([])
-  const total = options.total || ref(0)
+  const collection = options.collection
+  const total = options.total
   const offset = ref(0)
   const hasNextPage = computed(() => collection.value.length < total.value)
 
   const loadMore = () => (offset.value = collection.value.length)
-
-  const setCollection = (data) => {
-    collection.value = collection.value.concat(data.collection)
-    total.value = data.total
-  }
-
-  const reset = () => {
-    collection.value = []
-    total.value = 0
-    offset.value = 0
-  }
+  const reset = () => (offset.value = 0)
 
   return {
     collection,
@@ -28,7 +15,6 @@ const useLoadMorePagination = (options = {}) => {
     offset,
     hasNextPage,
     loadMore,
-    setCollection,
     reset,
   }
 }
