bc8b04e1615541fbf02a0ef705c6acccd2e20fe0
Andrzej Trzaska <atrzaska2@gmail.com>
2021-07-16 11:55:56 +0200

    Use button instead of div

--- src/views/Stories/components/Actions.vue
+++ src/views/Stories/components/Actions.vue
@@ -1,60 +1,60 @@
 <template>
   <div class="btn-group">
-    <div
+    <button
       v-if="story.state === 'unstarted'"
       class="btn btn-outline-secondary d-flex align-items-center"
       @click="updateStoryState(story, 'started')"
     >
       Start
-    </div>
-    <div
+    </button>
+    <button
       v-if="story.state === 'started'"
       class="btn btn-outline-secondary d-flex align-items-center"
       @click="updateStoryState(story, 'finished')"
     >
       Finish
-    </div>
-    <div
+    </button>
+    <button
       v-if="story.state === 'finished'"
       class="btn btn-outline-secondary d-flex align-items-center"
       @click="updateStoryState(story, 'delivered')"
     >
       Deliver
-    </div>
-    <div
+    </button>
+    <button
       v-if="story.state === 'delivered'"
       class="btn btn-outline-secondary d-flex align-items-center"
       @click="updateStoryState(story, 'accepted')"
     >
       Accept
-    </div>
-    <div
+    </button>
+    <button
       v-if="story.state === 'delivered'"
       class="btn btn-outline-secondary d-flex align-items-center"
       @click="updateStoryState(story, 'rejected')"
     >
       Reject
-    </div>
-    <div
+    </button>
+    <button
       v-if="story.state === 'rejected'"
       class="btn btn-outline-secondary d-flex align-items-center"
       @click="updateStoryState(story, 'unstarted')"
     >
       Restart
-    </div>
-    <div
+    </button>
+    <button
       v-if="story.state === 'accepted'"
       class="btn btn-outline-secondary d-flex align-items-center"
       @click="updateStoryState(story, 'unstarted')"
     >
       Restart
-    </div>
-    <div
+    </button>
+    <button
       class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split"
       data-bs-toggle="dropdown"
     >
       <img :src="threeDots" alt="pic" />
-    </div>
+    </button>
     <div class="dropdown-menu dropdown-menu-end">
       <router-link
         class="dropdown-item"
@@ -63,7 +63,7 @@
         Edit
       </router-link>
       <div class="dropdown-divider" />
-      <div class="dropdown-item" @click="onDelete(story)">Remove</div>
+      <button class="dropdown-item" @click="onDelete(story)">Remove</button>
     </div>
   </div>
 </template>
